57f7791cd2ef9fb304b5143471ae20d937a84f16
laser <lyrej37dxi@yahoo.com>
2024-11-03 22:56:19 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -11,16 +11,14 @@ module tt_um_a_4_array_multiplier (
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire       ena,      
+    input  wire       clk,      
+    input  wire       rst_n     
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-
-    input wire [3:0] m = ui_in[7:4]; // multiplicand
-    input wire [3:0] q = ui_in[3:0]; // multiplier
-    output wire [7:0] p; // product
+    wire [3:0] m = ui_in[7:4]; 
+    wire [3:0] q = ui_in[3:0]; 
+    wire [7:0] p;              
 
     wire [3:0] pp0, pp1, pp2, pp3;
     wire [4:0] sum1, sum2, sum3;
@@ -47,15 +45,22 @@ module tt_um_a_4_array_multiplier (
     full_add fa8 (sum2[2], pp3[2], carry3[1], p[5], carry3[2]);
     full_add fa9 (carry2[3], pp3[3], carry3[2], p[6], p[7]);
 
-  assign uo_out = p;
-    
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    assign uo_out = p;
+    assign uio_out = 0;
+    assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
-    wire _unused = &{ena, clk, rst_n, , uio_in, 1'b0};
+    wire _unused = &{ena, clk, rst_n, uio_in, 1'b0};
 
 endmodule
+
+module half_add (
+    input a, b,
+    output sum, cout
+);
+    assign sum = a ^ b;
+    assign cout = a & b;
+endmodule
+
 module full_add (
     input a, b, cin,
     output sum, cout
